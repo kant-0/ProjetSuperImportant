b24b5b19c66cf99224b1b70efd732caf9566a274
dblt-tim <timeodiebolt@gmail.com>
2026-06-10 14:36:12 +0200

    added json.c/h

---
+++ sim/source/json.c
@@ -0,0 +1,13 @@
+
+#include "json.h"
+#include "vector.h"
+#include <stdio.h>
+
+void write_json_to(FILE* out) {
+    // out must be set as write
+    fprintf(out, "{");
+
+
+
+    fprintf(out, "}");
+}
---
+++ sim/source/json.h
@@ -0,0 +1,9 @@
+
+#ifndef JSON_H
+#define JSON_H
+
+#include <stdio.h>
+
+void write_json_to(FILE* out);
+
+#endif
