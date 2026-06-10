55de39905eb604247fa5a002265c100f225723fd
kant-0 <quentinfred2006@outlook.fr>
2026-06-10 11:20:10 +0200

    planet structure

---
+++ sim/planet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "planet.h"
+#include "vector.h"
+
+planet * newPlanet() {
+    return NULL;
+}
+
+void pushElement(planet **previousPlanet, Vec3 newPosition, Vec3 newSpeed) {
+    planet* newPlanet = malloc(sizof(planet));
+    newPlanet->position = newPosition;
+    newPlanet->speed = newSpeed;
+    newPlanet->time = previousPlanet->time + 1;
+    
+    previousPlanet->next = newPlanet;
+    return
+}
---
+++ sim/planet.h
@@ -0,0 +1,17 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+#include "vector.h"
+
+typedef struct _planet {
+    Vec3 position;
+    Vec3 speed;
+    int time;
+    struct _planet *next;
+} planet;
+
+planet* newPlanet();
+
+void pushElement(planet **my_element, Vec3 newPosition, Vec3 newSpeed);
+
+#endif
