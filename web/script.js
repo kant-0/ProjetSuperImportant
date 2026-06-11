e332998e04e987d83d97c8c7fbf49493c4ff7c9b
dblt-tim <timeodiebolt@gmail.com>
2026-06-11 09:32:10 +0200

    base web

---
+++ web/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+function start(elm) {
+    new Response(elm.files[0]).json().then(json => {
+        console.log(json)
+        document.getElementById("sim-container").toggleAttribute("hidden")
+    }, err => {
+        // not json
+    })
+}
+
