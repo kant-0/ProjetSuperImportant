0d33e3bf017c2a79a22e345325c47e827bb827a6
dblt-tim <timeodiebolt@gmail.com>
2026-06-15 11:27:52 +0200

    fixed planet.c

--- sim/source/main.c
+++ sim/source/main.c
@@ -5,16 +5,77 @@
 #include "formula.h"
 #include "json.h"
 
+#define N_ITERATIONS 2000
+
 int main() {
-    Planet * mercure = newPlanet(
+    JsonSet* json = NULL;
+
+    char* planets[] = {"mercure", "venus", "terre", "mars", "jupiter", "saturne", "uranus", "neptune", "pluton"};
+    Vec3* pos0[] = {
+        newVec3(46001200.0e3, 0.0, 0.0),
+        newVec3(107476000.0e3, 0.0, 0.0),
+        newVec3(147098074.0e3, 0.0, 0.0),
+        newVec3(206655000.0e3, 0.0, 0.0),
+        newVec3(740680000.0e3, 0.0, 0.0),
+        newVec3(1349800000.0e3, 0.0, 0.0),
+        newVec3(2735000000.0e3, 0.0, 0.0),
+        newVec3(4459800000.0e3, 0.0, 0.0),
+        newVec3(4436824613.0e3, 0.0, 0.0)
+    };
+    Vec3* speed0[] = {
+        
+    };
+    
+    
+    Planet * mercure_e = newPlanet(
         newVec3(46001200.0e3, 0.0, 0.0),
         newVec3(0.0, 47362.0, 0.0), "mercure");
-    Planet * venus = newPlanet(
+    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(mercure_e, 1);
+    Planet * mercure_rk = newPlanet(
+        newVec3(46001200.0e3, 0.0, 0.0),
+        newVec3(0.0, 47362.0, 0.0), "mercure");
+    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(mercure_rk, 2);
+    Planet * mercure_ae = newPlanet(
+        newVec3(46001200.0e3, 0.0, 0.0),
+        newVec3(0.0, 47362.0, 0.0), "mercure");
+    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(mercure_ae, 3);
+    appendPlanetToJson(&json, 1, mercure_e);
+    appendPlanetToJson(&json, 2, mercure_rk);
+    appendPlanetToJson(&json, 3, mercure_ae);
+    
+    Planet * venus_e = newPlanet(
         newVec3(107476000.0e3, 0.0, 0.0), 
         newVec3(0.0, 35025.0, 0.0), "venus");
-    Planet * terre = newPlanet(
+    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(venus_e, 1);
+    Planet * venus_rk = newPlanet(
+        newVec3(107476000.0e3, 0.0, 0.0), 
+        newVec3(0.0, 35025.0, 0.0), "venus");
+    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(venus_rk, 2);
+    Planet * venus_ae = newPlanet(
+        newVec3(107476000.0e3, 0.0, 0.0), 
+        newVec3(0.0, 35025.0, 0.0), "venus");
+    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(venus_ae, 3);
+    appendPlanetToJson(&json, 1, venus_e);
+    appendPlanetToJson(&json, 2, venus_rk);
+    appendPlanetToJson(&json, 3, venus_ae);
+
+    
+    Planet * terre_e = newPlanet(
         newVec3(147098074.0e3, 0.0, 0.0), 
         newVec3(0.0, 29783.0, 0.0), "terre");
+    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(terre_e, 1);
+    Planet * terre_rk = newPlanet(
+        newVec3(147098074.0e3, 0.0, 0.0), 
+        newVec3(0.0, 29783.0, 0.0), "terre");
+    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(terre_rk, 2);
+    Planet * terre_ae = newPlanet(
+        newVec3(147098074.0e3, 0.0, 0.0), 
+        newVec3(0.0, 29783.0, 0.0), "terre");
+    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(terre_ae, 3);
+    appendPlanetToJson(&json, 1, terre_e);
+    appendPlanetToJson(&json, 2, terre_rk);
+    appendPlanetToJson(&json, 3, terre_ae);
+    
     Planet * mars = newPlanet(
         newVec3(206655000.0e3, 0.0, 0.0), 
         newVec3(0.0, 24080.0, 0.0), "mars");
@@ -34,14 +95,5 @@ int main() {
         newVec3(4436824613.0e3, 0.0, 0.0), 
         newVec3(0.0, 4740.0, 0.0), "pluton");
 
-    Planet * terre_test = newPlanet(
-        newVec3(147098074.0e3, 0.0, 0.0), 
-        newVec3(0.0, 29783.0, 0.0), "terre_test");
-    for (int i = 0; i < 100; i++) pushNextPlanetIteration(terre_test);
-    JsonObject* json = newJsonObject(SET, 0, 0, NULL, NULL);
-    appendPlanetToJson(&json->Set, 1, terre_test);
-    print_json_set(stdout, json->Set);
-
-    printf("\n");
     return 0;
 }
