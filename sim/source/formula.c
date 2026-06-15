32f28ebb6fec18cffd110c9a9b9b90af6127bce7
dblt-tim <timeodiebolt@gmail.com>
2026-06-15 08:34:28 +0200

    fix formula.c/h

--- sim/source/formula.c
+++ sim/source/formula.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 #include "vector.h"
@@ -36,7 +35,7 @@ Vec3* getNextPosVec3(Vec3* prevPos, Vec3* prevSpeed) {
     return addVec3(prevPos, prevSpeed);
 }
 
-Vec3 * eulerMethodSpeed(planet * trajectory, double deltaT) {
+Vec3 * eulerMethodSpeed(Planet * trajectory, double deltaT) {
     return addVec3( // vxt+1 = vxt + axt x Δt
         trajectory->speed, 
         scaleVec3(
@@ -46,7 +45,7 @@ Vec3 * eulerMethodSpeed(planet * trajectory, double deltaT) {
     ); 
 }
 
-Vec3 * eulerMethodPosition(planet * trajectory, double deltaT) {
+Vec3 * eulerMethodPosition(Planet * trajectory, double deltaT) {
     return addVec3( // xt+1 = xt + vxt x Δt
         trajectory->position, 
         scaleVec3(
@@ -56,7 +55,7 @@ Vec3 * eulerMethodPosition(planet * trajectory, double deltaT) {
     ); 
 }
 
-Vec3 * eulerAsymetricSpeed(planet * trajectory, double deltaT) {
+Vec3 * eulerAsymetricSpeed(Planet * trajectory, double deltaT) {
     Vec3 * posTn1 = eulerMethodPosition(trajectory, deltaT);
 
     return addVec3( // vxt+1 = vxt + axt+1 x Δt
@@ -68,11 +67,11 @@ Vec3 * eulerAsymetricSpeed(planet * trajectory, double deltaT) {
     ); 
 }
 
-Vec3 * eulerAsymetricPosition(planet * trajectory, double deltaT) {
+Vec3 * eulerAsymetricPosition(Planet * trajectory, double deltaT) {
     return eulerMethodPosition(trajectory, deltaT);
 }
 
-Vec3 * rungeKuttaSpeed(planet * trajectory, double deltaT) {
+Vec3 * rungeKuttaSpeed(Planet * trajectory, double deltaT) {
     Vec3 * k1r = scaleVec3(trajectory->speed, deltaT); // k1,r = Δt x vn
 
     Vec3 * kmidr = addVec3(trajectory->position, scaleVec3(k1r, 2.0)); // vn + k1,v/2 
@@ -85,13 +84,13 @@ Vec3 * rungeKuttaSpeed(planet * trajectory, double deltaT) {
     return addVec3(trajectory->speed, k2v);
 }
 
-Vec3 * rungeKuttaPosition(planet * trajectory, double deltaT) {
+Vec3 * rungeKuttaPosition(Planet * trajectory, double deltaT) {
     Vec3 * k1v = scaleVec3( // k1,v = Δt x an
         getAccelerationVec3(trajectory->position),
         deltaT
     );
 
-    Vec3 * kmidv = addVec3(trajectory->speed, scaleVec3(k1v, 2.0)) // rn + k1,r/2
+    Vec3 * kmidv = addVec3(trajectory->speed, scaleVec3(k1v, 2.0)); // rn + k1,r/2
 
     Vec3 * k2r = scaleVec3( // k2,r = Δt x kmidv
         kmidv, 
@@ -101,10 +100,12 @@ Vec3 * rungeKuttaPosition(planet * trajectory, double deltaT) {
     return addVec3(trajectory->position, k2r);
 }
 
-double potentialEnergy(planet * trajectory) {
+double potentialEnergy(Planet * trajectory) {
 
+    return 0;
 }
 
-double kineticEnergy(planet * trajectory, ) {
-    
+double kineticEnergy(Planet * trajectory) {
+
+    return 0;
 }
--- sim/source/formula.h
+++ sim/source/formula.h
@@ -1,6 +1,9 @@
 #ifndef FORMULA_H
 #define FORMULA_H
 
+#include "vector.h"
+#include "planet.h"
+
 double acceleration(double radius);
 
 Vec3* radius(Vec3* pos1, Vec3* pos2);
@@ -11,20 +14,20 @@ Vec3* getNextSpeedVec3(Vec3* prevSpeed, Vec3* prevAccel);
 
 Vec3* getNextPosVec3(Vec3* prevPos, Vec3* prevSpeed);
 
-Vec3 * eulerMethodSpeed(Vec3 * F, Vec3 * f, double deltaT);
+Vec3 * eulerMethodSpeed(Planet * trajectory, double deltaT);
 
-Vec3 * eulerMethodPosition(planet * trajectory, double deltaT);
+Vec3 * eulerMethodPosition(Planet * trajectory, double deltaT);
 
-Vec3 * eulerAsymetricSpeed(planet * trajectory, double deltaT);
+Vec3 * eulerAsymetricSpeed(Planet * trajectory, double deltaT);
 
-Vec3 * eulerAsymetricPosition(planet * trajectory, double deltaT):
+Vec3 * eulerAsymetricPosition(Planet * trajectory, double deltaT);
 
-Vec3 * rungeKuttaSpeed(planet * trajectory, double deltaT);
+Vec3 * rungeKuttaSpeed(Planet * trajectory, double deltaT);
 
-Vec3 * rungeKuttaPosition(planet * trajectory, double deltaT);
+Vec3 * rungeKuttaPosition(Planet * trajectory, double deltaT);
 
-double potentialEnergy(planet * trajectory);
+double potentialEnergy(Planet * trajectory);
 
-double kineticEnergy(planet * trajectory, );
+double kineticEnergy(Planet * trajectory);
 
 #endif
