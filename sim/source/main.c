f2e9b19d163963c348045426f684b399f9da68d4
dblt-tim <timeodiebolt@gmail.com>
2026-06-15 11:44:47 +0200

    data generation complete

--- sim/source/main.c
+++ sim/source/main.c
@@ -23,77 +23,33 @@ int main() {
         newVec3(4436824613.0e3, 0.0, 0.0)
     };
     Vec3* speed0[] = {
-        
+        newVec3(0.0, 47362.0, 0.0),
+        newVec3(0.0, 35025.0, 0.0),
+        newVec3(0.0, 29783.0, 0.0),
+        newVec3(0.0, 24080.0, 0.0),
+        newVec3(0.0, 13058.0, 0.0),
+        newVec3(0.0, 9640.0, 0.0),
+        newVec3(0.0, 6796.0, 0.0),
+        newVec3(0.0, 5432.0, 0.0),
+        newVec3(0.0, 4740.0, 0.0)
     };
-    
-    
-    Planet * mercure_e = newPlanet(
-        newVec3(46001200.0e3, 0.0, 0.0),
-        newVec3(0.0, 47362.0, 0.0), "mercure");
-    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(mercure_e, 1);
-    Planet * mercure_rk = newPlanet(
-        newVec3(46001200.0e3, 0.0, 0.0),
-        newVec3(0.0, 47362.0, 0.0), "mercure");
-    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(mercure_rk, 2);
-    Planet * mercure_ae = newPlanet(
-        newVec3(46001200.0e3, 0.0, 0.0),
-        newVec3(0.0, 47362.0, 0.0), "mercure");
-    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(mercure_ae, 3);
-    appendPlanetToJson(&json, 1, mercure_e);
-    appendPlanetToJson(&json, 2, mercure_rk);
-    appendPlanetToJson(&json, 3, mercure_ae);
-    
-    Planet * venus_e = newPlanet(
-        newVec3(107476000.0e3, 0.0, 0.0), 
-        newVec3(0.0, 35025.0, 0.0), "venus");
-    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(venus_e, 1);
-    Planet * venus_rk = newPlanet(
-        newVec3(107476000.0e3, 0.0, 0.0), 
-        newVec3(0.0, 35025.0, 0.0), "venus");
-    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(venus_rk, 2);
-    Planet * venus_ae = newPlanet(
-        newVec3(107476000.0e3, 0.0, 0.0), 
-        newVec3(0.0, 35025.0, 0.0), "venus");
-    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(venus_ae, 3);
-    appendPlanetToJson(&json, 1, venus_e);
-    appendPlanetToJson(&json, 2, venus_rk);
-    appendPlanetToJson(&json, 3, venus_ae);
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 3; j++) {
+            Planet* p = newPlanet(
+                pos0[i],
+                speed0[i],
+                planets[i]
+            );
+            for (int k = 0; k < N_ITERATIONS; k++) pushNextPlanetIteration(p, j+1);
+            appendPlanetToJson(&json, j+1, p);
+        }
+    }
+    FILE* f = fopen("data.json", "w");
 
-    
-    Planet * terre_e = newPlanet(
-        newVec3(147098074.0e3, 0.0, 0.0), 
-        newVec3(0.0, 29783.0, 0.0), "terre");
-    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(terre_e, 1);
-    Planet * terre_rk = newPlanet(
-        newVec3(147098074.0e3, 0.0, 0.0), 
-        newVec3(0.0, 29783.0, 0.0), "terre");
-    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(terre_rk, 2);
-    Planet * terre_ae = newPlanet(
-        newVec3(147098074.0e3, 0.0, 0.0), 
-        newVec3(0.0, 29783.0, 0.0), "terre");
-    for (int i = 0; i < N_ITERATIONS; i++) pushNextPlanetIteration(terre_ae, 3);
-    appendPlanetToJson(&json, 1, terre_e);
-    appendPlanetToJson(&json, 2, terre_rk);
-    appendPlanetToJson(&json, 3, terre_ae);
-    
-    Planet * mars = newPlanet(
-        newVec3(206655000.0e3, 0.0, 0.0), 
-        newVec3(0.0, 24080.0, 0.0), "mars");
-    Planet * jupiter = newPlanet(
-        newVec3(740680000.0e3, 0.0, 0.0), 
-        newVec3(0.0, 13058.0, 0.0), "jupiter");
-    Planet * saturne = newPlanet(
-        newVec3(1349800000.0e3, 0.0, 0.0), 
-        newVec3(0.0, 9640.0, 0.0), "saturne");
-    Planet * uranus = newPlanet(
-        newVec3(2735000000.0e3, 0.0, 0.0), 
-        newVec3(0.0, 6796.0, 0.0), "uranus");
-    Planet * neptune = newPlanet(
-        newVec3(4459800000.0e3, 0.0, 0.0), 
-        newVec3(0.0, 5432.0, 0.0), "neptune");
-    Planet * pluton = newPlanet(
-        newVec3(4436824613.0e3, 0.0, 0.0), 
-        newVec3(0.0, 4740.0, 0.0), "pluton");
+    if (!f) return 1;
 
+    print_json_set(f, json);
+
+    fclose(f);
     return 0;
 }
