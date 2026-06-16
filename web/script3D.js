0bb73d382ea42fa327601142941222638673adc5
kant-0 <quentinfred2006@outlook.fr>
2026-06-16 13:28:54 +0200

    noice

--- web/script3D.js
+++ web/script3D.js
@@ -4,26 +4,20 @@ let method = "method_euler";
 let bg;
 let speed = 1;
 let a = 0;
+let toggleTraj = true;
 
 const planets = {
-    sun:     { img: null, size: 30, rgb: null},
-    mercury: { img: null, size: 10, rgb: 'rgb(57, 57, 57)'},
-    venus:   { img: null, size: 20, rgb: 'rgb(230, 152, 89)'},
-    earth:   { img: null, size: 25, rgb: 'rgb(47, 106, 105)'},
-    mars:    { img: null, size: 20, rgb: 'rgb(153, 61, 0)'},
-    jupiter: { img: null, size: 30, rgb: 'rgb(176, 127, 53)'},
-    saturn:  { img: null, size: 30, rgb: 'rgb(176, 143, 54)'},
-    uranus:  { img: null, size: 25, rgb: 'rgb(85, 128, 170)'},
-    neptune: { img: null, size: 25, rgb: 'rgb(54, 104, 150)'},
-    pluto:   { img: null, size: 15, rgb: 'rgb(57, 57, 57)'}
+    sun:     { img: null, size: 30, rgb: null, show: true},
+    mercury: { img: null, size: 10, rgb: 'rgb(57, 57, 57)', show: true},
+    venus:   { img: null, size: 20, rgb: 'rgb(230, 152, 89)', show: true},
+    earth:   { img: null, size: 25, rgb: 'rgb(47, 106, 105)', show: true},
+    mars:    { img: null, size: 20, rgb: 'rgb(153, 61, 0)', show: true},
+    jupiter: { img: null, size: 30, rgb: 'rgb(176, 127, 53)', show: true},
+    saturn:  { img: null, size: 30, rgb: 'rgb(176, 143, 54)', show: true},
+    uranus:  { img: null, size: 25, rgb: 'rgb(85, 128, 170)', show: true},
+    neptune: { img: null, size: 25, rgb: 'rgb(54, 104, 150)', show: true},
+    pluto:   { img: null, size: 15, rgb: 'rgb(57, 57, 57)', show: true}
 };
-
-/*function drawTrajectory(name, method) {
-    for(let i=0; i < dataJSON[method][name].length ;i++){
-        if (name !== "sun")
-            point(dataJSON[method][name][i][0][0] / echelle, dataJSON[method][name][i][0][1] / echelle);
-    }
-}*/
 
 function drawTail(name) {
     const arr = dataJSON[method][name];
@@ -112,9 +106,12 @@ function draw() {
     drawPlanet3D("sun");
 
     for (const name of Object.keys(planets)) {
-        if (name !== "sun") {
+        if (name !== "sun" && planets[name].show) {
             drawPlanet3D(name);
-            drawTail(name);
+
+            if (toggleTraj) {
+                drawTail(name);
+            }
         }
     }
     
@@ -155,7 +152,23 @@ resetBtn.onclick = () => {
 }
 
 const slider = document.getElementById('slider');
+
 slider.oninput = () => {
     speed = floor(slider.value/10) + 1;
     console.log(slider.value);
 }
+
+for (const name of Object.keys(planets)) {
+    const id = "toggle" + name.charAt(0).toUpperCase() + name.slice(1);
+    const checkbox = document.getElementById(id);
+
+    if (checkbox) {
+        checkbox.onchange = () => {
+            planets[name].show = checkbox.checked;
+        };
+    }
+}
+
+document.getElementById('toggleTrajectory').onchange = (e) => {
+    toggleTraj = e.target.checked;
+};
